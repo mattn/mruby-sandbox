8e6d5b747457ec85f853f69ab044a97794f26bc7
mattn <mattn.jp@gmail.com>
2013-03-14 16:25:09 +0900

    Support windows

--- mrbgem.rake
+++ mrbgem.rake
@@ -1,4 +1,8 @@
 MRuby::Gem::Specification.new('mruby-sandbox') do |spec|
   spec.license = 'MIT'
   spec.authors = 'mattn'
+
+  if ENV['OS'] == 'Windows_NT'
+    spec.linker.libraries << ['winmm']
+  end
 end
--- src/mrb_sandbox.c
+++ src/mrb_sandbox.c
@@ -10,14 +10,33 @@
 #include <strings.h>
 #endif
 #include <ctype.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#include <mmsystem.h>
+#define SIGALRM 14
+typedef void (*sighandler_t)(int);
+sighandler_t f_sigalarm = NULL;
+void alarm(int sec) {
+	timeSetEvent(sec * 1000, 100, (LPTIMECALLBACK) f_sigalarm, 0, TIME_ONESHOT);
+}
+void _signal(int sig, sighandler_t t) {
+	if (sig == SIGALRM) {
+		f_sigalarm = t;
+	} else {
+		signal(sig, t);
+	}
+}
+#define signal(x, y) _signal(x, y)
+#endif
+
 typedef struct {
   mrb_state* mrb;
   mrbc_context *cxt;
+  int timeout;
 } mrb_sandbox_context;
 
 static void
@@ -112,17 +131,21 @@ mrb_sandbox_init(mrb_state* mrb, mrb_value self) {
 
   sc->mrb = my_mrb_open_allocf(allocf, NULL);
   sc->cxt = mrbc_context_new(sc->mrb);
+  sc->cxt->capture_errors = 1;
+  sc->timeout = 3;
   DATA_TYPE(self) = &mrb_sandbox_context_type;
   DATA_PTR(self) = sc;
   return self;
 }
 
+static mrb_state* current_mrb = NULL;
 static mrb_state* last_mrb = NULL;
-void timeout(int n) {
-  if (last_mrb) {
-    mrb_state* mrb = last_mrb;
-    mrb_raise(mrb, E_RUNTIME_ERROR, "Timeout");
-  }
+static void
+f_timeout(int sig) {
+  last_mrb->exc = (struct RObject*) mrb_object(mrb_funcall(
+    current_mrb,
+    mrb_obj_value(mrb_class_obj_get(last_mrb, "RuntimeError")),
+      "new", 1, mrb_str_new_cstr(current_mrb, "Timeout")));
 }
 
 static mrb_value
@@ -143,21 +166,22 @@ mrb_sandbox_eval(mrb_state* mrb, mrb_value self) {
   mrb_parser_parse(parser, sc->cxt);
   if (0 < parser->nerr) {
     mrb_parser_free(parser);
-    //mrb_raisef(mrb, E_RUNTIME_ERROR, "line %d: %s\n", parser->error_buffer[0].lineno, parser->error_buffer[0].message);
     return mrb_nil_value();
   }
   n = mrb_generate_code(sc->mrb, parser);
   mrb_parser_free(parser);
+
+  current_mrb = mrb;
   last_mrb = sc->mrb;
-  signal(SIGALRM, timeout);
-  alarm(3);
+  signal(SIGALRM, f_timeout);
+  alarm(sc->timeout);
   result = mrb_run(sc->mrb,
     mrb_proc_new(sc->mrb, sc->mrb->irep[n]),
     mrb_top_self(sc->mrb));
   signal(SIGALRM, SIG_IGN);
   int ai = mrb_gc_arena_save(mrb);
   if (sc->mrb->exc) {
-    obj = mrb_funcall(sc->mrb, mrb_obj_value(sc->mrb->exc), "inspect", 0);
+    obj = mrb_funcall(mrb, mrb_obj_value(sc->mrb->exc), "inspect", 0);
     mrb_gc_arena_restore(mrb, ai);
     sc->mrb->exc = 0;
     return mrb_str_new(mrb, RSTRING_PTR(obj), RSTRING_LEN(obj));
@@ -167,12 +191,25 @@ mrb_sandbox_eval(mrb_state* mrb, mrb_value self) {
   return mrb_str_new(mrb, RSTRING_PTR(obj), RSTRING_LEN(obj));
 }
 
+static mrb_value
+mrb_sandbox_timeout_get(mrb_state *mrb, mrb_value self) {
+  return mrb_fixnum_value(((mrb_sandbox_context*) DATA_PTR(self))->timeout);
+}
+
+static mrb_value
+mrb_sandbox_timeout_set(mrb_state *mrb, mrb_value self) {
+  mrb_get_args(mrb, "i", &(((mrb_sandbox_context*) DATA_PTR(self))->timeout));
+  return mrb_fixnum_value(((mrb_sandbox_context*) DATA_PTR(self))->timeout);
+}
+
 void
 mrb_mruby_sandbox_gem_init(mrb_state* mrb) {
   struct RClass* _class_sandbox = mrb_define_class(mrb, "Sandbox", mrb->object_class);
   MRB_SET_INSTANCE_TT(_class_sandbox, MRB_TT_DATA);
   mrb_define_method(mrb, _class_sandbox, "initialize", mrb_sandbox_init, ARGS_NONE());
   mrb_define_method(mrb, _class_sandbox, "eval", mrb_sandbox_eval, ARGS_REQ(1));
+  mrb_define_method(mrb, _class_sandbox, "timeout", mrb_sandbox_timeout_get, ARGS_NONE());
+  mrb_define_method(mrb, _class_sandbox, "timeout=", mrb_sandbox_timeout_set, ARGS_REQ(1));
 }
 
 void
