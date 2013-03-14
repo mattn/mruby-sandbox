165563d52a1a1af779c4b4fe0abb457f99958c77
mattn <mattn.jp@gmail.com>
2013-03-14 11:15:15 +0900

    Use to_s instead of inspect

--- src/mrb_sandbox.c
+++ src/mrb_sandbox.c
@@ -142,7 +142,9 @@ mrb_sandbox_eval(mrb_state* mrb, mrb_value self) {
   parser->lineno = 1;
   mrb_parser_parse(parser, sc->cxt);
   if (0 < parser->nerr) {
-    mrb_raisef(mrb, E_RUNTIME_ERROR, "line %d: %s\n", parser->error_buffer[0].lineno, parser->error_buffer[0].message);
+    mrb_parser_free(parser);
+    //mrb_raisef(mrb, E_RUNTIME_ERROR, "line %d: %s\n", parser->error_buffer[0].lineno, parser->error_buffer[0].message);
+    return mrb_nil_value();
   }
   n = mrb_generate_code(sc->mrb, parser);
   mrb_parser_free(parser);
@@ -153,12 +155,15 @@ mrb_sandbox_eval(mrb_state* mrb, mrb_value self) {
     mrb_proc_new(sc->mrb, sc->mrb->irep[n]),
     mrb_top_self(sc->mrb));
   signal(SIGALRM, SIG_IGN);
+  int ai = mrb_gc_arena_save(mrb);
   if (sc->mrb->exc) {
     obj = mrb_funcall(sc->mrb, mrb_obj_value(sc->mrb->exc), "inspect", 0);
+    mrb_gc_arena_restore(mrb, ai);
     sc->mrb->exc = 0;
     return mrb_str_new(mrb, RSTRING_PTR(obj), RSTRING_LEN(obj));
   }
-  obj = mrb_funcall(sc->mrb, result, "inspect", 0);
+  obj = mrb_funcall(sc->mrb, result, "to_s", 0);
+  mrb_gc_arena_restore(mrb, ai);
   return mrb_str_new(mrb, RSTRING_PTR(obj), RSTRING_LEN(obj));
 }
 
